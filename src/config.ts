b87ea6052d2887cc3433755270083fe728db4cf8
nacho <ignacio.bsso@hotmail.com>
2022-02-02 19:51:49 -0300

    add config and env

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+export default {
+    "db": {
+        "user": null,
+        "pass": null,
+        "host": "localhost",
+        "port": "27017",
+        "database": "local",
+        "authSource": null
+    },
+    "host": {
+        "url": "<server-url>",
+        "port": "3000"
+    },
+    "jwt": {
+        "secretOrKey": "secret",
+        "expiresIn": 36000000
+    },
+    "mail":{
+        "host": 'smtp.gmail.com',
+        "port":  465,
+        "secure": true,
+        "user": "ayudarnos.org@gmail.com",
+        "pass": "eanu fvoh wcde pbal"
+    }
+  }
